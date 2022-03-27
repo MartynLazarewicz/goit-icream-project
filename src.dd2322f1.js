301a5749f9228ccdd6486e4eee8837ab03a47909
MartynLazarewicz <75955903+MartynLazarewicz@users.noreply.github.com>
2022-03-27 06:51:50 +0000

    Deploying to gh-pages from @ MartynLazarewicz/goit-icream-project@c328b0609d9492c14a40117e0e70d03981d9583b 🚀

--- src.dd2322f1.js
+++ src.dd2322f1.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/dotActive.svg":[["dotActive.1d62f7d4.svg","ngHB"],"ngHB"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-icream-project/src.e2d09f4f.js.map
+//# sourceMappingURL=/goit-icream-project/src.dd2322f1.js.map
